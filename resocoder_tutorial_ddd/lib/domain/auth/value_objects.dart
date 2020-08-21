a19e2fc00f85eab17204bfda9c5713f90974cf3b
Simon Wongsuwarn <wongsuwarn@gmail.com>
2020-08-21 20:03:45 +0100

    create email and password value objects

---
+++ resocoder_tutorial_ddd/lib/domain/auth/value_objects.dart
@@ -0,0 +1,35 @@
+// perform validation within type instead of in presentation layer
+import 'package:dartz/dartz.dart';
+import 'package:resocoder_tutorial_ddd/domain/core/failures.dart';
+import 'package:resocoder_tutorial_ddd/domain/core/value_objects.dart';
+import 'package:resocoder_tutorial_ddd/domain/core/value_validators.dart';
+
+class EmailAddress extends ValueObject {
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(
+    this.value,
+  );
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
+
+class Password extends ValueObject<String> {
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
